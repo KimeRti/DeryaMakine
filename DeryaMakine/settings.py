e5c2815b85f4308cb66c4c163abe76df90adb191
Ahmet <merterli_ahmet18@hotmail.com>
2023-07-18 19:48:39 +0300

    s

--- DeryaMakine/settings.py
+++ DeryaMakine/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-muy_48i^fe5l6cs+odk#!y$2a4qq^#+t)8t=zci_oyj2z#6l#e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["deryamakine.com","www.deryamakine.com"]
 
 
 # Application definition
@@ -121,10 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'static')
-     ]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+#STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+#     ]
+#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type

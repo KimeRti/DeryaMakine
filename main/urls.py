91fe3fc143ea35da25f8b9bc6f2e90a2b075f07e
Ahmet <merterli_ahmet18@hotmail.com>
2023-07-25 19:37:57 +0300

    fix

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('index.html',views.home_view,name="home"),
     path("about", views.about_view ,name="about"),
     path("contact", views.contact_view ,name="contact"),
+    path("brands", views.brands_view ,name="brands"),
+    path("privacy", views.privacy_view ,name="privacy"),
+    path("terms", views.terms_view ,name="terms"),
+    path("usage", views.usage_view ,name="usage"),
 ]
--- main/views.py
+++ main/views.py
@@ -13,6 +13,24 @@ def contact_view(request):
     return render(request, "main/contact.html", {'url': 'contact'})
 
 
+def brands_view(request):
+    return render(request, "main/brands.html", {'url': 'brands'})
+
+
+def privacy_view(request):
+    return render(request, "main/privacy.html", {'url': 'privacy'})
+
+
+def terms_view(request):
+    return render(request, "main/terms.html", {'url': 'terms'})
+
+
+def usage_view(request):
+    return render(request, "main/usage.html", {'url': 'usage'})
+
+
+
+
 
 
 

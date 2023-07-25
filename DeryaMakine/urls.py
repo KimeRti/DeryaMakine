8fc976c313fefe1cd8e258d2e59e17e02adb26b4
Ahmet <merterli_ahmet18@hotmail.com>
2023-07-25 15:19:58 +0300

    fix

--- DeryaMakine/urls.py
+++ DeryaMakine/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('account/', include('account.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.production.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 

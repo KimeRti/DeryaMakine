3e3fc4f2b603e12452d2fd08e2365c4472f85851
Ahmet <merterli_ahmet18@hotmail.com>
2023-07-25 15:33:03 +0300

    fix

--- DeryaMakine/settings/development.py
+++ DeryaMakine/settings/development.py
@@ -8,3 +8,4 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'static/')
      ]
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
--- DeryaMakine/urls.py
+++ DeryaMakine/urls.py
@@ -23,6 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('account/', include('account.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 

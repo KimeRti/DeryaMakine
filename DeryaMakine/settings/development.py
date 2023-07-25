e9e19ef3f249d47bfec6418098157198f0f1e972
Ahmet <merterli_ahmet18@hotmail.com>
2023-07-25 15:04:56 +0300

    fixx

--- DeryaMakine/settings/development.py
+++ DeryaMakine/settings/development.py
@@ -4,4 +4,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '../../static/'),]
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, '../../static')
+     ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')

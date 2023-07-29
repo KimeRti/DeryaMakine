f3209d387b71fd819ba397bed9ae84f01c4de8e6
Ahmet <merterli_ahmet18@hotmail.com>
2023-07-29 11:22:48 +0300

    fix

--- DeryaMakine/settings/base.py
+++ DeryaMakine/settings/base.py
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.request',  # Yeni satırı ekleyin
+                'django.template.context_processors.request',
             ],
         },
     },
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 

d3264a0b8dcdb7168adf65cdabfa4fa67f9a0fd3
Ahmet <merterli_ahmet18@hotmail.com>
2023-07-30 10:54:00 +0300

    fix

--- DeryaMakine/settings/production.py
+++ DeryaMakine/settings/production.py
@@ -4,4 +4,5 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['www.deryamakine.com', 'deryamakine.com']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = 'DeryaMakine/static/'
+STATICFILES_DIRS = []

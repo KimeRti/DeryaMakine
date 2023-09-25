b179fbe345c6ecc6049266953350af71a7eeebce
Ahmet <merterli_ahmet18@hotmail.com>
2023-09-25 21:07:10 +0300

    fix

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.products_view, name="products"),
+    path('productdetail/<int:pk>', views.product_detail_view, name='productdetail'),
 ]
--- product/views.py
+++ product/views.py
@@ -12,4 +12,13 @@ def products_view(request):
     return render(request, "products/products_1.html", data)
 
 
+def product_detail_view(request, pk):
+    product = Product.objects.get(id=pk)
+    url = 'products'
+    data = {
+        "product": product,
+        "url": url
+    }
+    return render(request, "products/product_details.html", data)
+
 

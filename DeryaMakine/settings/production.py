5b8d05d20743e69efc2a054ee652e7c6a166cd60
Ahmet <merterli_ahmet18@hotmail.com>
2023-10-16 10:59:57 +0300

    csrf fix2

--- DeryaMakine/settings/production.py
+++ DeryaMakine/settings/production.py
@@ -7,3 +7,6 @@ ALLOWED_HOSTS = ['www.deryamakine.com', 'deryamakine.com']
 STATIC_ROOT = 'DeryaMakine/static/'
 
 CSRF_TRUSTED_ORIGINS = ['www.deryamakine.com', 'deryamakine.com']
+CORS_ALLOWED_ORIGINS = ['www.deryamakine.com', 'deryamakine.com']
+CORS_ALLOWED_ORIGIN_REGEXES = ['www.deryamakine.com', 'deryamakine.com']
+CORS_ALLOW_ALL_ORIGINS = ['www.deryamakine.com', 'deryamakine.com']

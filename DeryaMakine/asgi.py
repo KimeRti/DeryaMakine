190a756de400931787e9e44bb27b72e02a2cb3a2
Ahmet <112516305+KimeRti@users.noreply.github.com>
2024-05-07 22:13:31 +0300

    Update asgi.py

--- DeryaMakine/asgi.py
+++ DeryaMakine/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DeryaMakine.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DeryaMakine.settings')
 
 application = get_asgi_application()

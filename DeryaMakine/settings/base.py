16a069cd0108a73b8ad40476e3d9251b96c239f9
Ahmet <112516305+KimeRti@users.noreply.github.com>
2024-05-20 21:20:27 +0300

    Update base.py

--- DeryaMakine/settings/base.py
+++ DeryaMakine/settings/base.py
@@ -115,8 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
-MEDIA_URL = os.path.join(BASE_DIR, 'media/')
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
